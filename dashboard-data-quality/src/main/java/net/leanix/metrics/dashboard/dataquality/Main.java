42095b1dd2ffabca6c1c64ce7f7fb249138646d2
Michael Szwarc <cmszwarc@gmail.com>
2017-03-12 01:13:11 +0100

    rework #3

--- dashboard-data-quality/src/main/java/net/leanix/metrics/dashboard/dataquality/Main.java
+++ dashboard-data-quality/src/main/java/net/leanix/metrics/dashboard/dataquality/Main.java
@@ -4,59 +4,47 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public final class Main {
 
 	public static final void main(String[] args) {
 		// define cli's
-		CommandLineParser parser = new DefaultParser();
 		Options options = new Options();
-		options.addOption("h", "host", true, "The host, e.g. app.leanix.net (required).");
-		options.addOption("w", "workspace", true, "The leanIX workspace (required).");
+		options.addOption(Option.builder("h").longOpt("host").required().hasArg()
+				.desc("The host, e.g. app.leanix.net (required).").build());
+		options.addOption(Option.builder("w").longOpt("workspace").required().hasArg()
+				.desc("The leanIX workspace (required).").build());
 		/*
-		 * TODO workspace id could be retrieved in different ways - query
-		 * leanix-mtm-sdk-java -> ApiTokensApi.getApiTokens(...) - query
-		 * undocumented rest api '/config' call - as cli option
+		 * TODO workspace id could be retrieved in different ways
+		 * 
+		 * - query leanix-mtm-sdk-java -> ApiTokensApi.getApiTokens(...)
+		 * 
+		 * - query undocumented rest api '/config' call
+		 * 
+		 * - as cli option (check if workspace and id belong to each other?)
 		 */
-		options.addOption("wid", "workspaceid", true, "The leanIX workspace id for metrics (required).");
-		options.addOption("t", "token", true, "The API token  (required).");
-		options.addOption("d", "debug", false, "Sets the debug mode (optional flag).");
-		options.addOption("?", "help", false, "Prints this help and returns (optional flag).");
+		options.addOption(Option.builder("wid").longOpt("workspaceid").required().hasArg()
+				.desc("The leanIX workspace id for metrics (required).").build());
+		options.addOption(
+				Option.builder("t").longOpt("token").required().hasArg().desc("The API token  (required).").build());
+		options.addOption(Option.builder("d").longOpt("debug").desc("Enables the debug mode.").build());
+		options.addOption(Option.builder("?").longOpt("help").desc("Prints this help and returns.").build());
+		CommandLineParser parser = new DefaultParser();
 		HelpFormatter formatter = new HelpFormatter();
-		// read cli values
 		try {
+			// read cli values
 			CommandLine line = parser.parse(options, args);
 			if (args.length == 0 || line.hasOption("help")) {
-				printHelp(formatter, options);
-				System.exit(0);
+				printHelp(formatter, options, 0);
 			}
 			String host = read(line, 'h');
 			String workspace = read(line, 'w');
 			String workspaceId = read(line, "wid");
 			String token = read(line, 't');
 			boolean debug = line.hasOption('d');
-			// validate cli values
-			if (host == null) {
-				System.out.println("'host' is missing.\n");
-				printHelp(formatter, options);
-				System.exit(-1);
-			}
-			if (workspace == null) {
-				System.out.println("'workspace' is missing.\n");
-				printHelp(formatter, options);
-				System.exit(-1);
-			}
-			if (workspaceId == null) {
-				System.out.println("'workspaceId' is missing.\n");
-				printHelp(formatter, options);
-				System.exit(-1);
-			}
-			if (token == null) {
-				System.out.println("'token' is missing.\n");
-				printHelp(formatter, options);
-				System.exit(-1);
-			}
 			// create clients
 			net.leanix.api.common.ApiClient apiClient = new net.leanix.api.common.ApiClientBuilder()
 					.withBasePath(String.format("https://%s/%s/api/v1", host, workspace)).withTokenProviderHost(host)
@@ -65,12 +53,13 @@ public final class Main {
 					.withBasePath(String.format("https://%s/services/metrics/v1", host)).withTokenProviderHost(host)
 					.withApiToken(token).withDebugging(debug).build();
 			// run import
-			ImportJob job = new ImportJob(apiClient, metricsClient, workspaceId, debug);
-			job.run();
+			new ImportJob(apiClient, metricsClient, workspaceId, debug).run();
+		} catch (ParseException e) {
+			System.out.println(e.getLocalizedMessage() + "\n");
+			printHelp(formatter, options, -1);
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
-			printHelp(formatter, options);
-			System.exit(-1);
+			System.exit(-2);
 		}
 	}
 
@@ -82,7 +71,8 @@ public final class Main {
 		return line.hasOption(opt) ? line.getOptionValue(opt).trim() : null;
 	}
 
-	private static void printHelp(HelpFormatter formatter, Options options) {
+	private static void printHelp(HelpFormatter formatter, Options options, int exitCode) {
 		formatter.printHelp("java -jar dashboard-data-quality.jar", options, true);
+		System.exit(exitCode);
 	}
 }
